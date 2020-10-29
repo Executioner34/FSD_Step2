3d3ee7818fa3dae41c5ccc9c749248362ecd292a
Alexander Gorlenko <goral92@inbox.ru>
2020-10-29 19:09:00 +0400

    add plugin air datepicker and card calendar

---
+++ src/components/textForm/textForm.js
@@ -0,0 +1,8 @@
+import '../../../node_modules/air-datepicker/dist/js/datepicker.min';
+import '../../../node_modules/air-datepicker/dist/css/datepicker.min.css';
+
+$('#my-datepicker').datepicker({
+	navTitles: {
+		days: 'MM yyyy'
+	}
+})
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import './components/dropdown/dropdown';
+import './components/textForm/textForm'
 import './pages/ui-kit.styles.scss';
 import './components/like-button/like-button';
